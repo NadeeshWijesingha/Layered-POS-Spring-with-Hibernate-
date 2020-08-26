258e18e28bd5d17caa91a1d0eeafc957ceb5394c
NadeeshWijesingha <nadeeshwijesingha@gmail.com>
2020-08-26 22:40:10 +0530

    Change package location to lk.ijse.dep.pos and add AppConfig class

--- src/dao/SuperDAO.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import org.hibernate.Session;
-
-public interface SuperDAO {
-    public void setSession(Session session);
-}
---
+++ src/lk/ijse/dep/pos/dao/custom/impl/OrderDetailDAOImpl.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep.pos.dao.custom.impl;
+
+import lk.ijse.dep.pos.dao.CrudDAOImpl;
+import lk.ijse.dep.pos.dao.custom.OrderDetailDAO;
+import lk.ijse.dep.pos.entity.OrderDetail;
+import lk.ijse.dep.pos.entity.OrderDetailPK;
+
+import java.util.List;
+
+public class OrderDetailDAOImpl extends CrudDAOImpl<OrderDetail, OrderDetailPK> implements OrderDetailDAO {
+
+    @Override
+    public List<OrderDetail> findAll() throws Exception {
+        return session.createQuery("FROM OrderDetail", OrderDetail.class).list();
+    }
+}
