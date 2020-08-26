80f61dbb506d3ecb9390d79471bc2f658577a44d
NadeeshWijesingha <nadeeshwijesingha@gmail.com>
2020-08-26 21:47:37 +0530

    Initial Commit

---
+++ src/dao/custom/impl/OrderDetailDAOImpl.java
@@ -0,0 +1,17 @@
+package dao.custom.impl;
+
+import dao.CrudDAOImpl;
+import dao.custom.OrderDetailDAO;
+import entity.OrderDetail;
+import entity.OrderDetailPK;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class OrderDetailDAOImpl extends CrudDAOImpl<OrderDetail, OrderDetailPK> implements OrderDetailDAO {
+
+    @Override
+    public List<OrderDetail> findAll() throws Exception {
+        return session.createQuery("FROM OrderDetail", OrderDetail.class).list();
+    }
+}
